62b8b39473592f753f6bb6753360f8157105f082
nixoxo <nico.axtmann95@gmail.com>
2015-06-18 09:37:43 +0200

    Test for database failed...

--- src/test/java/persistence/ChildDAOTest.java
+++ src/test/java/persistence/ChildDAOTest.java
@@ -1,7 +1,11 @@
 package persistence;
 
 import de.pm.mindcloud.MindCloudApplication;
+import de.pm.mindcloud.persistence.DatabaseService;
+import de.pm.mindcloud.persistence.domain.Child;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -14,4 +18,11 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = MindCloudApplication.class)
 public class ChildDAOTest {
 
+    @Autowired
+    private DatabaseService databaseService;
+
+    @Test
+    public void createChild() throws Exception {
+        databaseService.insert(new Child("yolo"));
+    }
 }
