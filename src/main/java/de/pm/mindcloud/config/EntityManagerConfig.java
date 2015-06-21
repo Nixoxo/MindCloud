d647befa586f6093d19d012598615f0c25e56302
nixoxo <nico.axtmann95@gmail.com>
2015-06-21 18:09:22 +0200

    Preparing for heroku deploy, added enviroment vars

--- src/main/java/de/pm/mindcloud/config/EntityManagerConfig.java
+++ src/main/java/de/pm/mindcloud/config/EntityManagerConfig.java
@@ -8,6 +8,8 @@ import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by samuel on 09.05.15.
@@ -19,9 +21,33 @@ public class EntityManagerConfig {
     @Value("${database.config}")
     private String persistenceUnit;
 
+    @Value("${database.host}")
+    private String host;
+
+    @Value("${database.port}")
+    private String port;
+
+    @Value("${database.database}")
+    private String database;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
+
     @PostConstruct
     public void init() {
-        emf = Persistence.createEntityManagerFactory(persistenceUnit);
+        Map<String, String> props = new HashMap<>();
+        props.put("hibernate.ogm.datastore.provider", "MONGODB");
+        props.put("hibernate.ogm.datastore.create_database", "true");
+        props.put("hibernate.ogm.datastore.host", host);
+        props.put("hibernate.ogm.datastore.port", port);
+        props.put("hibernate.ogm.datastore.database", database);
+        props.put("hibernate.ogm.datastore.username", username);
+        props.put("hibernate.ogm.datastore.password", password);
+        emf = Persistence.createEntityManagerFactory(persistenceUnit, props);
     }
 
     @Bean
